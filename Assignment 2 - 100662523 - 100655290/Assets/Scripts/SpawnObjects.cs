bf51feb0ce26da979f39f756cbf17b252897fa09
matthewdadoun <matthewdadoun@gmail.com>
2019-10-06 18:39:07 -0400

    Updated build

--- Assignment 2 - 100662523 - 100655290/Assets/Scripts/SpawnObjects.cs
+++ Assignment 2 - 100662523 - 100655290/Assets/Scripts/SpawnObjects.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SpawnObjects : MonoBehaviour
     
 {
+    public Transform deletePos;
     public GameObject Cube;
     public GameObject Sphere;
     public GameObject Plane;
+    public PhysicMaterial bounce;
     public Material cubeMaterial;
     public Material sphereMaterial;
     public Material planeMaterial;
-    int selectedObject = 1;
+    int selectedObject = 0;
     // Update is called once per frame
     void Update()
     {
@@ -21,8 +23,11 @@ public class SpawnObjects : MonoBehaviour
             {
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 cube.GetComponent<Renderer>().material = cubeMaterial;
-
                 cube.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Destroy(cube);
+                }
             }
             if (selectedObject == 2)
             {
@@ -33,11 +38,11 @@ public class SpawnObjects : MonoBehaviour
             }
             if (selectedObject == 3)
             {
-               GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-               plane.GetComponent<Renderer>().material = sphereMaterial;
-               plane.AddComponent<Rigidbody>();
-               plane.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-            }  
+                GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                plane.GetComponent<Renderer>().material = sphereMaterial;
+                plane.transform.localScale = new Vector3(1.0f, 0.01f, 1.0f);
+                plane.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -51,6 +56,10 @@ public class SpawnObjects : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             selectedObject = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            selectedObject = 0;
         }
     }
         }
